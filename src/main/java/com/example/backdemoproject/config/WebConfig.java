14ae43c10dc949d702b1c11c0eba327231d30375
sh0304 <shsh0304@gmail.com>
2025-11-07 16:46:11 +0900

    fix: CORS 에러 수정 - 개발 서버에서 api 요청 시, cors 발생하여 개발 서버 주소 추가

--- src/main/java/com/example/backdemoproject/config/WebConfig.java
+++ src/main/java/com/example/backdemoproject/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:3000", "http://sohyun-front-demo-project-dev-bucket.s3-website.ap-northeast-2.amazonaws.com")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
